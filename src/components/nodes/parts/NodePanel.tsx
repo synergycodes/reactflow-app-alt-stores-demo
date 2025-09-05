ce0d269797d77d093be661ed42bf81925baa9375
Szymon Tondowski <szymon.tondowski@synergycodes.com>
2025-09-05 09:44:37 +0200

    Complex context for diagram 1

--- src/components/nodes/parts/NodePanel.tsx
+++ src/components/nodes/parts/NodePanel.tsx
@@ -10,7 +10,7 @@ const NodePanel = ({ children, className = "", isSelected = false }: Props) => {
   return (
     <div
       className={cn(
-        "min-w-[200px]",
+        "min-w-[200px] relative",
         "rounded-xl border border-zinc-50 bg-white py-2 px-4 shadow",
         "duration-500 hover:shadow-md hover:border-zinc-400",
         "outline-dashed outline-transparent outline-2 outline-offset-12",
--- src/versions/diagram-1-default/features/connectionMaker/provider.tsx
+++ src/versions/diagram-1-default/features/connectionMaker/provider.tsx
@@ -1,14 +1,68 @@
-import { useState, type PropsWithChildren } from "react";
+import { useCallback, useState, type PropsWithChildren } from "react";
 import { ConnectionMakerContext } from "./context";
+import type { ConnectionMakerStatus } from "./types";
+import { addEdge, useReactFlow, type Edge } from "@xyflow/react";
+
+type ContextData = {
+  status: ConnectionMakerStatus;
+  sourceNodeId: string | null;
+};
+
+const initialState: ContextData = {
+  status: "setSource",
+  sourceNodeId: null,
+};
 
 export const ConnectionMakerProvider = ({ children }: PropsWithChildren) => {
-  const [sourceNodeId, setSourceNodeId] = useState<string | null>(null);
+  const { setEdges } = useReactFlow();
+  const [{ status, sourceNodeId }, setContextData] =
+    useState<ContextData>(initialState);
+
+  const cancelLinking = useCallback(() => {
+    setContextData(initialState);
+  }, []);
+
+  const handleSetSourceNodeId = useCallback(
+    (sourceNodeId: string | null) => {
+      if (!sourceNodeId) {
+        cancelLinking();
+
+        return;
+      }
+
+      setContextData({
+        status: "setTarget",
+        sourceNodeId,
+      });
+    },
+    [cancelLinking]
+  );
+
+  const handleSetTargetNodeId = useCallback(
+    (targetNodeId: string | null) => {
+      if (sourceNodeId && targetNodeId) {
+        const newEdge: Edge = {
+          id: crypto.randomUUID(),
+          source: sourceNodeId,
+          target: targetNodeId,
+        };
+
+        setEdges((edges) => addEdge<Edge>(newEdge, edges));
+
+        setContextData(initialState);
+      }
+    },
+    [setEdges, sourceNodeId]
+  );
 
   return (
     <ConnectionMakerContext.Provider
       value={{
+        status,
         sourceNodeId,
-        setSourceNodeId,
+        cancelLinking,
+        setSourceNodeId: handleSetSourceNodeId,
+        setTargetNodeId: handleSetTargetNodeId,
       }}
     >
       {children}
