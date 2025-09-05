5aba833c8394964464ec7844125b285cb1025f8f
Szymon Tondowski <szymon.tondowski@synergycodes.com>
2025-09-05 09:48:53 +0200

    Complex context for ver 2

--- src/versions/diagram-2-zustand/features/connectionMaker/components/ConnectButton.tsx
+++ src/versions/diagram-2-zustand/features/connectionMaker/components/ConnectButton.tsx
@@ -1,88 +1,63 @@
 import { useGetSelectedNodeId } from "@/components/hooks/useGetSelectedNodeId";
 import IconEdgeStart from "@/components/icons/IconEdgeStart";
 import Button from "@/components/ui/Button";
-import { useCallback, useMemo } from "react";
 import { useConnectionMarkerContext } from "../hooks/useConnectionMarkerContext";
 import IconEdgeEnd from "@/components/icons/IconEdgeEnd";
-import { addEdge, useReactFlow, type Edge } from "@xyflow/react";
 
 type Props = {
   className?: string;
 };
 
 const ConnectButton = ({ className }: Props) => {
-  const { setEdges } = useReactFlow();
   const selectedNodeId = useGetSelectedNodeId();
-  const { sourceNodeId, setSourceNodeId } = useConnectionMarkerContext();
-
-  const type: "setSource" | "removeSource" | "setTarget" = useMemo(() => {
-    if (sourceNodeId) {
-      if (sourceNodeId === selectedNodeId) {
-        return "removeSource";
-      }
-
-      if (selectedNodeId) {
-        return "setTarget";
-      }
-
-      return "removeSource";
-    }
-
-    return "setSource";
-  }, [selectedNodeId, sourceNodeId]);
-
-  const handleClick = useCallback(() => {
-    if (type === "setSource") {
-      setSourceNodeId(selectedNodeId);
-
-      return;
-    }
-
-    if (type === "removeSource") {
-      setSourceNodeId(null);
-
-      return;
-    }
-
-    if (sourceNodeId && selectedNodeId) {
-      const newEdge: Edge = {
-        id: crypto.randomUUID(),
-        source: sourceNodeId,
-        target: selectedNodeId,
-      };
-
-      setEdges((edges) => addEdge<Edge>(newEdge, edges));
-
-      setSourceNodeId(null);
-    }
-  }, [type, sourceNodeId, selectedNodeId, setSourceNodeId, setEdges]);
+  const {
+    status,
+    cancelLinking,
+    sourceNodeId,
+    setSourceNodeId,
+    setTargetNodeId,
+  } = useConnectionMarkerContext();
 
   if (!selectedNodeId && !sourceNodeId) {
     return null;
   }
 
-  return (
-    <Button className={className} onClick={handleClick}>
-      {type === "setSource" && (
-        <>
-          <IconEdgeStart className="size-6" />
-          <span>Link to other node</span>
-        </>
-      )}
-      {type === "removeSource" && (
-        <>
-          <IconEdgeStart className="size-6" />
-          <span>Cancel linking</span>
-        </>
-      )}
-      {type === "setTarget" && (
-        <>
+  if (status === "setSource") {
+    return (
+      <Button
+        className={className}
+        onClick={() => setSourceNodeId(selectedNodeId)}
+      >
+        <IconEdgeStart className="size-6" />
+        <span>Link to other node</span>
+      </Button>
+    );
+  }
+
+  if (status === "setTarget") {
+    const isSelectedAndNotSource =
+      selectedNodeId && selectedNodeId !== sourceNodeId;
+    if (isSelectedAndNotSource) {
+      return (
+        <Button
+          className={className}
+          onClick={() => setTargetNodeId(selectedNodeId)}
+        >
           <IconEdgeEnd className="size-6" />
-          <span>Create a link</span>
-        </>
-      )}
-    </Button>
-  );
+          <span>Link to other node</span>
+        </Button>
+      );
+    }
+
+    return (
+      <Button className={className} onClick={cancelLinking}>
+        <IconEdgeStart className="size-6" />
+        <span>Cancel linking</span>
+      </Button>
+    );
+  }
+
+  return null;
 };
 
 export default ConnectButton;
