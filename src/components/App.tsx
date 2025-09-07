88251804317de36140a66af5cfa7b55c520ac6cb
Szymon Tondowski <szymon.tondowski@synergycodes.com>
2025-09-07 22:04:50 +0200

    Title for the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { Link, Route, Switch } from "wouter";
+import { usePathname } from "wouter/use-browser-location";
 
 import { cn } from "@/utils/cn";
 
@@ -8,14 +9,25 @@ import Flow3 from "@/versions/diagram-3-zustand-no-actions/components/Flow";
 import Flow4 from "@/versions/diagram-4-zustand-actions/components/Flow";
 
 import IconVersions from "./icons/IconVersions";
+import { useEffect } from "react";
 
 function App() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const pathTitle = pathname.replaceAll("-", " ").replaceAll("/", "");
+
+    document.title = pathTitle
+      ? `${pathTitle} | ReactFlow performance demo`
+      : "ReactFlow performance demo";
+  }, [pathname]);
+
   return (
     <Switch>
-      <Route path="/diagram-1-default" component={Flow1} />
-      <Route path="/diagram-2-zustand" component={Flow2} />
-      <Route path="/diagram-3-zustand-no-actions" component={Flow3} />
-      <Route path="/diagram-4-zustand-actions" component={Flow4} />
+      <Route path="/1-default" component={Flow1} />
+      <Route path="/2-zustand" component={Flow2} />
+      <Route path="/3-zustand-no-actions" component={Flow3} />
+      <Route path="/4-zustand-actions" component={Flow4} />
       <Route>
         <div className="h-screen flex flex-col justify-center">
           <main className="text-center w-[500px] mx-auto max-w-screen">
@@ -31,20 +43,20 @@ function App() {
             <nav className="text-left">
               <ul>
                 <li>
-                  <Link href="/diagram-1-default">1. Default</Link>
+                  <Link href="/1-default">1. Default</Link>
                   <p className="leading-7 text-sm text-zinc-500 px-10 py-5">
                     Standard ReactFlow implementation without an external store,
                     with drag-and-drop and custom edge creation implemented.
                   </p>
                 </li>
                 <li>
-                  <Link href="/diagram-2-zustand">2. Zustand store</Link>
+                  <Link href="/2-zustand">2. Zustand store</Link>
                   <p className="leading-7 text-sm text-zinc-500 px-10 py-5">
                     Standard ReactFlow implementation with one external store.
                   </p>
                 </li>
                 <li>
-                  <Link href="/diagram-3-zustand-no-actions">
+                  <Link href="/3-zustand-no-actions">
                     3. Zustand store without actions
                   </Link>
                   <p className="leading-7 text-sm text-zinc-500 px-10 py-5">
@@ -53,7 +65,7 @@ function App() {
                   </p>
                 </li>
                 <li>
-                  <Link href="/diagram-4-zustand-actions">
+                  <Link href="/4-zustand-actions">
                     4. Zustand store with actions
                   </Link>
                   <p className="leading-7 text-sm text-zinc-500 px-10 py-5">
