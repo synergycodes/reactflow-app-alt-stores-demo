a30a6e0de8f48a5729cc6f4127bd17fc8eda4db1
Szymon Tondowski <szymon.tondowski@synergycodes.com>
2026-09-03 12:31:57 +0200

    Documentation and favicons added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,31 +10,45 @@ import Flow3 from "@/versions/diagram-3-zustand-store-actions/components/Flow";
 import Flow4 from "@/versions/diagram-4-zustand-actions/components/Flow";
 
 import IconVersions from "./icons/IconVersions";
+import IconGithub from "./icons/IconGithub";
+import IconYoutube from "./icons/IconYoutube";
 
 function App() {
   const pathname = usePathname();
 
   useEffect(() => {
-    const pathTitle = pathname.replaceAll("-", " ").replaceAll("/", "");
+    const pathTitle = pathname.replace('reactflow-app-alt-stores-demo', '').replaceAll("-", " ").replaceAll("/", "").trim();
 
     document.title = pathTitle
-      ? `${pathTitle} | ReactFlow performance demo`
-      : "ReactFlow performance demo";
+      ? `${pathTitle} | React Flow + Zustand: No-Store Actions Demo | Synergy Codes`
+      : "React Flow + Zustand: No-Store Actions Demo | Synergy Codes";
   }, [pathname]);
 
   return (
     <Switch>
-      <Route path="/reactflow-app-alt-stores-demo/1-default" component={Flow1} />
-      <Route path="/reactflow-app-alt-stores-demo/2-zustand-and-context" component={Flow2} />
-      <Route path="/reactflow-app-alt-stores-demo/3-zustand-store-actions" component={Flow3} />
-      <Route path="/reactflow-app-alt-stores-demo/4-zustand-actions" component={Flow4} />
+      <Route
+        path="/reactflow-app-alt-stores-demo/1-default"
+        component={Flow1}
+      />
+      <Route
+        path="/reactflow-app-alt-stores-demo/2-zustand-and-context"
+        component={Flow2}
+      />
+      <Route
+        path="/reactflow-app-alt-stores-demo/3-zustand-store-actions"
+        component={Flow3}
+      />
+      <Route
+        path="/reactflow-app-alt-stores-demo/4-zustand-actions"
+        component={Flow4}
+      />
       <Route>
         <div className="h-screen flex flex-col justify-center">
-          <main className="text-center w-[500px] py-4 mx-auto max-w-screen max-h-dvw overflow-auto">
+          <main className="text-center w-[500px] p-2 mx-auto max-w-screen max-h-dvh overflow-auto">
             <h1
               className={cn(
                 "flex items-center gap-2 mb-8",
-                "text-zinc-500 text-2xl tracking-wider"
+                "text-zinc-500 text-2xl tracking-wider",
               )}
             >
               <IconVersions className="size-8 text-lime-700" />
@@ -60,7 +74,7 @@ function App() {
                 </li>
                 <li className="border border-zinc-400 rounded-lg p-5">
                   <Link
-                    href="/reactflow-app-alt-stores-demo/ 2-zustand-and-context"
+                    href="/reactflow-app-alt-stores-demo/2-zustand-and-context"
                     className="font-semibold text-zinc-900 hover:text-lime-700 duration-100 tracking-wider"
                   >
                     2. Zustand store and contexts
@@ -108,6 +122,37 @@ function App() {
                   </p>
                 </li>
               </ul>
+              <div className="flex justify-center items-center flex-wrap text-xs gap-4 mt-5">
+                <a
+                  href="https://github.com/synergycodes/reactflow-app-alt-stores-demo"
+                  className="flex items-center gap-2 border border-zinc-400 rounded-lg p-2 text-zinc-500 hover:text-lime-700 duration-100 tracking-wider"
+                  target="_blank"
+                >
+                  <IconGithub className="size-6 text-[black]" />
+                  Repository
+                </a>
+                <a
+                  href="https://www.youtube.com/watch?v=41FsulrcrQg"
+                  className="flex items-center gap-2 border border-zinc-400 rounded-lg p-2 text-zinc-500 hover:text-lime-700 duration-100 tracking-wider"
+                  target="_blank"
+                >
+                  <IconYoutube className="size-6 text-[red]" />
+                  App walkthrough
+                </a>
+
+                <p className="text-zinc-400 text-[10px]">
+                  Created by{" "}
+                  <a
+                    href="https://www.synergycodes.com/"
+                    className="font-semibold text-black tracking-wider"
+                    title="Synergy Codes"
+                    target="_blank"
+                  >
+                    Synergy Codes
+                  </a>
+                  .
+                </p>
+              </div>
             </nav>
           </main>
         </div>
